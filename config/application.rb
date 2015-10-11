2d588902435ba43352b666e0a4d9038c56822428
Bradley Gohman <bradley.s.gohman@gmail.com>
2015-10-10 21:34:29 -0500

    initial setup

--- config/application.rb
+++ config/application.rb
@@ -8,13 +8,18 @@ Bundler.require(*Rails.groups)
 
 module ListingService
   class Application < Rails::Application
+    config.generators do |g|
+      g.test_framework :rspec, fixtures: true, view_specs: false, helper_specs: false,
+      routing_specs: false, controller_specs: true, request_specs: false
+      g.fixtures_replacement :factory_girl, dir: "spec/factories"
+    end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    # config.time_zone = 'Central Time (US & Canada)'
+    config.time_zone = 'Central Time (US & Canada)'
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
@@ -24,3 +29,4 @@ module ListingService
     config.active_record.raise_in_transactional_callbacks = true
   end
 end
+
